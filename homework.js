5d29f37d5d73ac14d5978436ffb8a7b272f5cdf9
James Monks <J.monks23@gmail.com>
2020-07-20 19:22:06 +0100

    #2 completed

--- homework.js
+++ homework.js
@@ -1,15 +1,32 @@
-const scenario = {
-    murderer: 'Miss Scarlet',
-    room: 'Library',
-    weapon: 'Rope'
-  };
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+//   };
   
-  const declareMurderer = function() {
-    return `The murderer is ${scenario.murderer}.`;
-  }
+//   const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+//   }
   
-  const verdict = declareMurderer();
-  console.log(verdict);
+//   const verdict = declareMurderer();
+//   console.log(verdict);
   
   // The murderer is Miss Scarlet.
   // Scenario, declareMurderer, verdict are all available globally via const
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The murderer is Professor Plum.
+// murder is a const and can't be changed to Mrs. Peacock
