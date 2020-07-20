04395eb552234abe7a1e74956eaaccd0051efdd6
James Monks <J.monks23@gmail.com>
2020-07-20 19:34:39 +0100

    #3 completed

--- homework.js
+++ homework.js
@@ -14,19 +14,38 @@
   // The murderer is Miss Scarlet.
   // Scenario, declareMurderer, verdict are all available globally via const
 
-const murderer = 'Professor Plum';
+// const murderer = 'Professor Plum';
 
-const changeMurderer = function() {
-  murderer = 'Mrs. Peacock';
-}
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
 
-const declareMurderer = function() {
-  return `The murderer is ${murderer}.`;
-}
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
 
-changeMurderer();
-const verdict = declareMurderer();
-console.log(verdict);
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
 
 // The murderer is Professor Plum.
 // murder is a const and can't be changed to Mrs. Peacock
+
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// First Verdict: Mrs. Peacock
+// declareMurderer function is being console logged which returns the murderer as Mrs. Peacock first
+// Second Verdict: Professor Plum
+// no function is being called and the second verdict is calling the let variable thats being assigned outside of any function
+
